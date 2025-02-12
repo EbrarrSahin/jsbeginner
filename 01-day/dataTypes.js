d6917f57cff6738f858c925ad8b1c2331bb3f310
Ebrar <ebrarsahin004@gmail.com>
2025-02-12 15:05:53 +0300

    Day 1 finished

---
+++ 01-day/dataTypes.js
@@ -0,0 +1,4 @@
+console.log(typeof'ebrar')
+console.log(typeof 2)
+console.log(typeof '2')
+console.log(typeof true)
---
+++ 01-day/variable.js
@@ -0,0 +1,16 @@
+
+let firsName= 'Ebrar' // string
+let lastName= 'Şahin' // string
+let isMarried= false // boolean
+let country= 'Türkiye' // string
+let age= 20 // int
+let firsName= 'Ebrar'  lastName= 'Şahin'  isMarried= false  country= 'Türkiye'  age= 20
+
+// after you comment these lines
+
+
+let myAge= 20
+let yourAge= 25
+console.log("I am", myAge, "years old.")
+console.log("You are", yourAge, "yers old.")
+
