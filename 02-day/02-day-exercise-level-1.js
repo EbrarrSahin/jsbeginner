70cd649dadbfbcc912d6e60d8d52f24f35634f94
Ebrar <ebrarsahin004@gmail.com>
2025-02-13 22:20:05 +0300

    02 day finished

---
+++ 02-day/02-day-exercise-level-1.js
@@ -0,0 +1,35 @@
+let challange = "30 Days Of JavaScript"
+console.log(challange)
+console.log(challange.length)
+console.log(challange.toUpperCase())
+console.log(challange.toLowerCase())
+console.log(challange.substring(3,7))
+console.log(challange.substring(3,21))
+console.log(challange.includes('Script'))
+console.log(challange.split(''))
+console.log(challange.split(' '))
+
+let string = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+console.log(string.split(', '))
+
+console.log(challange.replace('JavaScript' , 'Python'))
+console.log(challange.charAt(15))
+console.log(challange.charCodeAt(11))
+console.log(challange.indexOf('a'))
+console.log(challange.lastIndexOf('a'))
+
+let string2 = 'You cannot end a sentence with because'
+console.log(string2.indexOf('because'))
+console.log(string2.lastIndexOf('because'))
+console.log(string2.search('because'))
+
+console.log(challange.trim())
+console.log(challange.startsWith('30'))
+console.log(challange.endsWith('JavaScript'))
+console.log(challange.match(/a/))
+
+let string3 = '30 Days Of '
+console.log(string3.concat('JavaScript'))
+
+
+console.log(challange.repeat())
--- 02-day/strings.js
+++ 02-day/strings.js
@@ -32,7 +32,7 @@ console.log(js.toUpperCase()) // Abc to ABC
 console.log(js.toLowerCase()) // Abc to abc
 
 // substr() : it takes 2 arguments, starting and stopping index numbers to slice
-// console.log(js.substr(4,6)) // why it looks weird? : The error indicates that the substr function is not defined in the current environment. Instead, slice or substring should be used.
+// console.log(js.sub str(4,6)) // why it looks weird? : The error indicates that the substr function is not defined in the current environment. Instead, slice or substring should be used.
 
 // substring() : same but doesnt count the last index
 console.log(js.substring(4,6)) 
@@ -50,4 +50,54 @@ console.log(string2.split(', ')) // removes blank too
 let string3= "    30 Days of JavaScript    "
 console.log(string3.trim()) 
 
-// includes() : 
+// includes() : if it has substring its true, otherwise its false
+console.log(string.includes('Days'))
+console.log(string.includes('days')) // case sensitive
+console.log(string.includes('Script'))
+console.log(string.includes('me'))
+
+// replace() : for replacing. needs 2 parameters
+console.log(string.replace('JavaScript', 'Python'))
+
+// charAt() : returns index
+let lastIndex = string.length -1
+console.log(string.charAt(lastIndex))
+
+// charCodeAt() : returns char code as ASCII
+console.log(string.charCodeAt(3))
+
+// indexOf() : if substring exists it returns first position else -1
+console.log(string.indexOf('D'))
+console.log(string.indexOf('script'))
+console.log(string.indexOf('Script'))
+
+// lastIndexOf() : returns last position
+
+//concat() : 
+let fname = 'Mar'
+console.log(fname.concat("vin"))
+
+// endsWith() : boolean
+let string4 = 'Love is the most powerful feeling in the world'
+
+console.log(string4.endsWith('world'))
+console.log(string4.endsWith('love'))
+
+// search() : it takes a substring as an argument and it returns the index of the first match. The search value can be a string or a regular expression pattern.
+
+// match() : it takes a substring or regular expression pattern as an argument and it returns an array if there is match if not it returns null. Let us see how a regular expression pattern looks like. It starts with / sign and ends with / sign.
+let string5 = 'love'
+let patternOne = /love/     // with out any flag
+let patternTwo = /love/gi   // g-means to search in the whole text, i - case insensitive !!!
+
+
+// parseInt() : if there is an integer in a string, this func converts it to string
+console.log(parseInt("42"))
+
+// Number() : string to int or float
+console.log(Number("3.14"))
+// + : Number() but faster verse 
+console.log(+true)
+console.log(+false)
+console.log(+null)
+console.log(+"")
