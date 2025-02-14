fc3a1c672f0cca09617044199d6e0d4aa75ac12b
Ebrar <ebrarsahin004@gmail.com>
2025-02-14 22:56:37 +0300

    day 3

---
+++ 03-day/booleans.js
@@ -0,0 +1,36 @@
+let isTired = true
+let isHungry = false
+let trueValue = 4 > 3
+let falseValue = 4 < 3
+
+// Truthy Values 
+/*
+true
+"Hello"
+"0"
+" "
+42	0
+-42
+3.14
+Infinity
+-Infinity
+[]
+{}
+function() {}
+*/
+
+// Falsy Values
+/*
+false
+0
+-0
+0n
+"" : empty strings
+null
+undefined
+NaN
+*/
+
+// if a function is not turning a value, it will be undefined
+let firstName
+console.log(firstName)
---
+++ 03-day/operators.js
@@ -0,0 +1,25 @@
+// Asignment Operators (=)
+let x = 8
+let y = 4
+x += y
+console.log(x)
+x -= y
+console.log(x)
+x *= y
+console.log(x)
+x /= y
+console.log(x)
+x %= y
+console.log(x)
+x **= y // exponential
+console.log(x)
+
+const PI = 3.14
+let radius = 100
+
+const areaOfCircle = PI * radius *  radius
+console.log(`Are of the circle is ${areaOfCircle}`)
+
+// Comparison Operators >.<
+
+console.log(2<3)
