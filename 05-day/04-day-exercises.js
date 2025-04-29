cc1a3e34ac41fabd98385e2fc67afd739fb9f0fe
Ebrar <ebrarsahin004@gmail.com>
2025-04-29 20:23:03 +0300

    day 4

---
+++ 05-day/04-day-exercises.js
@@ -0,0 +1,17 @@
+//Level 1
+
+const arr = Array()
+
+const arr2 = [11,12,13,14,15]
+
+console.log(arr2.length)
+let firstNum = arr2[0]
+console.log(firstNum)
+
+let midNum = arr2[2]
+console.log(midNum)
+
+let lastNum = arr2[4]
+console.log(lastNum)
+
+const mixedDataTypes = [1, 2, 'yogurt', 'rice', 'everything', 'nice']
